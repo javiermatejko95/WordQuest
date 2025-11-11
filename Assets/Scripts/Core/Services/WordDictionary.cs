02ac0d13392a89af78976daf96de9c9426517bd4
Javier Matejko <javiermatejko-externo@tgacompany.com>
2025-11-11 20:58:06 -0300

    Added word dictionary

---
+++ Assets/Scripts/Core/Services/WordDictionary.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class WordDictionary
+{
+    public HashSet<string> Words { get; private set; }
+    public HashSet<string> WordsClean { get; private set; }
+
+    public WordDictionary()
+    {
+        Load();
+    }
+
+    public void Load()
+    {
+        TextAsset file = Resources.Load<TextAsset>("words5_clean");
+        Words = new HashSet<string>(file.text
+            .Split('\n')
+            .Select(word => word.Trim()));
+
+        WordsClean = new HashSet<string>(
+        file.text
+            .Split('\n')
+            .Select(word => StringUtils.RemoveDiacritics(word.Trim().ToUpper()))
+        );
+
+        Debug.Log($"?? Palabras cargadas: {Words.Count}");
+    }
+
+    public bool IsValid(string word)
+    {
+        return WordsClean.Contains(word.ToUpper());
+    }
+}

48eb97caba737c6095ed4f503080620972de1385
Javier Matejko <javiermatejko-externo@tgacompany.com>
2025-11-24 16:22:37 -0300

    Added win by win attempt in stats model

--- Assets/Scripts/Core/Model/StatsModel.cs
+++ Assets/Scripts/Core/Model/StatsModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 [Serializable]
 public class StatsModel
@@ -8,4 +9,15 @@ public class StatsModel
     public int CurrentStreak;
     public int MaxStreak;
     public int VictoryPercentage;
+    public int CurrentAttempt;
+
+    public Dictionary<int, int> WinsByAttempt = new Dictionary<int, int>();
+
+    public StatsModel()
+    {
+        WinsByAttempt = new Dictionary<int, int>();
+
+        for (int i = 1; i <= 6; i++)
+            WinsByAttempt[i] = 0;
+    }
 }

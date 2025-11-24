f8a3edb8ed2ca76a0194df47c63f529def7383f4
Javier Matejko <javiermatejko-externo@tgacompany.com>
2025-11-24 16:22:18 -0300

    Added more events to StatsEvents script

--- Assets/Scripts/Core/Events/StatsEvents.cs
+++ Assets/Scripts/Core/Events/StatsEvents.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 public static class StatsEvents
 {
-    public static Action<bool> OnEndGame;
-    public static Func<StatsModel> OnGetSats;
+    public static Action<bool, WordModel> OnEndGame;
+    public static Func<StatsModel> OnGetStats;
+    public static Action OnTogglePopup;
+    public static Action OnResetStats;
+    public static Func<Dictionary<int, float>> OnGetAttemptsPercentages;
 }
